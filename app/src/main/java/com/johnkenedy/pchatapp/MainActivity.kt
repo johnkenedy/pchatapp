9c957581eed846a156b72dc5883adf7bf1f16acd
JohnKenedy <john.kenedy232@gmail.com>
2025-12-01 20:32:59 -0300

    set up navigation

--- app/src/main/java/com/johnkenedy/pchatapp/MainActivity.kt
+++ app/src/main/java/com/johnkenedy/pchatapp/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.johnkenedy.pchatapp.navigation.ChatNavHost
 import com.johnkenedy.pchatapp.ui.theme.PChatAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PChatAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                ChatNavHost()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PChatAppTheme {
-        Greeting("Android")
     }
 }
