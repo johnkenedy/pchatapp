9e17d6e6f0c6ac54e50475ea395518a4243634ec
JohnKenedy <john.kenedy232@gmail.com>
2025-12-06 22:37:28 -0300

    import resources

---
+++ app/src/main/java/com/johnkenedy/pchatapp/ui/feature/splash/SplashScreen.kt
@@ -0,0 +1,23 @@
+package com.johnkenedy.pchatapp.ui.feature.splash
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.johnkenedy.pchatapp.ui.theme.PChatAppTheme
+
+@Composable
+fun SplashRoute() {
+
+}
+
+@Composable
+fun SplashScreen() {
+
+}
+
+@Composable
+@Preview
+fun SplashScreenPreview() {
+    PChatAppTheme {
+        SplashScreen()
+    }
+}
