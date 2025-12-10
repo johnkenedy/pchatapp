71e6d6b6638522855bfc3b7d5a23a5d89eb8ee43
JohnKenedy <john.kenedy232@gmail.com>
2025-12-09 21:46:43 -0300

    type-safety

--- app/src/main/java/com/johnkenedy/pchatapp/navigation/ChatNavHost.kt
+++ app/src/main/java/com/johnkenedy/pchatapp/navigation/ChatNavHost.kt
@@ -1,25 +1,44 @@
 package com.johnkenedy.pchatapp.navigation
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.toRoute
 import com.johnkenedy.pchatapp.ui.feature.splash.SplashRoute
+import kotlinx.coroutines.delay
+import kotlinx.serialization.Serializable
 
-const val SPLASH_ROUTE = "splash"
-const val SIGN_IN_ROUTE = "signIn"
-const val SIGN_UP_ROUTE = "signUp"
+@Serializable
+object SplashRoute
 
+@Serializable
+data class SignInRoute(
+    val email: String,
+    val password: String
+)
+
+@Serializable
+object SignUpRoute
 
 @Composable
 fun ChatNavHost() {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = SPLASH_ROUTE) {
-        composable(SPLASH_ROUTE) {
+    NavHost(navController = navController, startDestination = SplashRoute) {
+        composable<SplashRoute> {
             SplashRoute()
+            LaunchedEffect(Unit) {
+                delay(2000)
+                navController.navigate(SignInRoute("John@test.com", "123"))
+            }
         }
-        composable(SIGN_IN_ROUTE) {}
-        composable(SIGN_UP_ROUTE) {}
+        composable<SignInRoute> {
+            val signInRoute = it.toRoute<SignInRoute>()
+            Text(text = "email: ${signInRoute.email} password: ${signInRoute.password}")
+        }
+        composable<SignUpRoute> {}
     }
 }
